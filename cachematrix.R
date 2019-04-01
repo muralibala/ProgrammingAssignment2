efc75e8d81ae0d5d6bd0f76846b440d2bd33b378
Murali Bala <i.am.muralibala@gmail.com>
2019-04-01 19:48:24 -0400

    Init commit - changes to makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,18 @@
 ## functions do
 
 ## Write a short comment describing this function
+##cache the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y;
+    inv <<- NULL;
+  }
+  get <- function() return(x);
+  setinv <- function(inverse) inv <<- inverse;
+  getinv <- function() return(inv);
+  return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
 
 
